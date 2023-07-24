b5c274e403a99845f68b95ca37a5412de7afe5f8
DmitriyRadozhitskiy <d.rado1991@gmail.com>
2023-07-24 10:55:46 +0300

    update

--- celery/celery_app.py
+++ celery/celery_app.py
@@ -15,7 +15,7 @@ app.conf.timezone = 'Europe/Moscow'
 app.conf.beat_schedule = {
     'update_tokens': {
         'task': 'tasks.new_week',
-        'schedule': crontab(hour=10, minute=57, day_of_week=1),
+        'schedule': crontab(hour=10, minute=57, day_of_week=1), # check
     },
     # 'ask_openai': {
     #     'task': 'tasks.openai',
