274abde80f28d2439378b5609b27b5084b1e19d7
DmitriyRadozhitskiy <d.rado1991@gmail.com>
2023-07-24 11:02:28 +0300

    update

--- gs_sheets.py
+++ gs_sheets.py
@@ -16,9 +16,9 @@ def add_results_to_gs(part, result):
     sheet = client.open('Дима Р (Персональная программа Кроссфит)')
     proga = sheet.worksheet('ПРОГА')
     part_dict = json.loads(part)
-    if proga.cell(25, part_dict["Result"][1]).value:
-        var = proga.cell(25, part_dict["Result"][1]).value + f'\n Часть {part_dict["Result"][2]}'
-        proga.update_cell(25, part_dict["Result"][1], f'{var}\n {result} \n')
+    if proga.cell(23, part_dict["Result"][1]).value:
+        var = proga.cell(23, part_dict["Result"][1]).value + f'\n Часть {part_dict["Result"][2]}'
+        proga.update_cell(23, part_dict["Result"][1], f'{var}\n {result} \n')
     else:
         var = f'Часть {part_dict["Result"][2]}'
-        proga.update_cell(25, part_dict["Result"][1], f'{var}\n {result} \n')
+        proga.update_cell(23, part_dict["Result"][1], f'{var}\n {result} \n')
